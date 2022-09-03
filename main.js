c8da4ef661bf30327800e4d2820c6c1a5c1a9391
Vishal S <98063471+Vishal12s@users.noreply.github.com>
2022-09-03 10:42:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var outputCard = document.querySelector(".outputCard");
+var userEmail = document.getElementById("umail");
+var userPassword = document.getElementById("upass");
+var firstName = document.getElementById("ufirstname");
+var lastName = document.getElementById("ulastname");
+var fullName = firstName + lastName;
+var male = document.getElementById("male");
+var female = document.getElementById("female");
+// var India = document.getElementById("India");
+// var Japan = document.getElementById("Japan");
+// var USA= document.getElementById("USA");
+// var China = document.getElementById("China");
+// var Australia = document.getElementById("Australia");
+// var Dubai = document.getElementById("Dubai");
+var weblink = document.getElementById("weblink");
+var skill = document.getElementById("skill");
+var form = document.querySelector("form");
+var submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", function() {
+    // if(!fullName.value || !userEmail.value || !userPassword.value) {
+    //     alert("Please enter all fields !");
+    // }
+    // else {
+        outputCard.innerHTML += `
+        
+        <div class="card">
+        <div class="card-body">
+        <ul>
+        <li><h4 class="title"><b>Name:</b> ${firstName.value} ${lastName.value}</h4></li>
+        <li><p class="text"><b>Gender :</b>Male</p></li>
+        <li> <p class="text"><b>E-mail:</b> ${userEmail.value}</p></li>
+        <li><p class="text"><b>Permanent Address:</b> ${weblink.value}</p></li>
+        <li><p class="text"><b>Stream:</b> ${skill.value}</p></li>
+        </ul>
+        </div>
+        </div>`
+
+        alert("Student Enrolled Successfully")
+        form.reset()
+    // }
+})
